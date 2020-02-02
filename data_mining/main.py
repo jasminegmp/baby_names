e2b96a4b40354ce33b8ab6e5570a70b67c308a5a
Jasmine <jasmine.kim.18@gmail.com>
2020-02-01 22:54:31 -0800

    Cleaned up data, ready to integrate into D3/React

--- data_mining/main.py
+++ data_mining/main.py
@@ -1,3 +1,36 @@
 import pandas as pd
-data = pd.read_csv('/names/yob1880.txt', sep=",", header=None)
-print data
+import os
+
+
+def create_ss_df():
+    rootdir = './names'
+    df = pd.DataFrame()
+    year = 1880
+    for subdir, dirs, files in os.walk(rootdir):
+        for file in files:
+            title =  os.path.join(subdir, file)
+            new_data = pd.read_csv(title, sep=",", header=None)
+            new_data['year'] = year
+            year += 1
+            df = df.append(new_data)
+    df.to_pickle("./ss_name_df.pkl")
+
+
+def reorder_df():
+    
+    unpickled_df = pd.read_pickle("./ss_name_df.pkl")
+    cols = unpickled_df.columns.tolist()
+    cols = cols[-1:] + cols[:-1]
+    unpickled_df = unpickled_df[cols]
+    unpickled_df.columns = ['date', 'name', 'gender', 'value']
+    unpickled_df.to_pickle("./ss_name_df.pkl")
+    print unpickled_df
+
+def write_to_csv():
+    unpickled_df = pd.read_pickle("./ss_name_df.pkl")
+    unpickled_df = unpickled_df[unpickled_df.value > 10000]
+    unpickled_df.to_csv('ss_name_df.csv', index=False)
+    
+#create_ss_df()
+#reorder_df()
+#write_to_csv()
---
+++ src/D3Viz.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+class D3Viz extends React.Component{
+
+    constructor(props) {
+        super(props);
+        
+        this.state = {
+        };
+    }
+
+    componentDidMount() {
+      }
+
+      render() {
+       
+        return (
+            <div>Hi!</div>
+        )
+    }
+  }
+  
+  export default D3Viz;
+  
